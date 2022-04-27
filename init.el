e62b0becb72299bb4940bcf9ba833558dac4168b
shirdekel <49865192+shirdekel@users.noreply.github.com>
2022-04-27 10:47:05 +1000

    Count org subheadings

--- init.el
+++ init.el
@@ -735,6 +735,30 @@ contains an ID, that ID will be replaced with a new one."
   (put 'evil-ex-history 'history-length 50)
   (put 'kill-ring 'history-length 25)
 
+  ;; Count number of subheadings in org
+  ;; From https://emacs.stackexchange.com/a/10276
+  (defun org-number-of-subentries (&optional pos match scope level)
+    "Return number of subentries for entry at POS.
+MATCH and SCOPE are the same as for `org-map-entries', but
+SCOPE defaults to 'tree.
+By default, all subentries are counted; restrict with LEVEL."
+    (interactive)
+    (save-excursion
+      (goto-char (or pos (point)))
+      ;; If we are in the middle ot an entry, use the current heading.
+      (org-back-to-heading t)
+      (let ((maxlevel (when (and level (org-current-level))
+                        (+ level (org-current-level)))))
+        (message "%s subentries"
+                 (1- (length
+                      (delq nil
+                            (org-map-entries
+                             (lambda ()
+                               ;; Return true, unless below maxlevel.
+                               (or (not maxlevel)
+                                   (<= (org-current-level) maxlevel)))
+                             match (or scope 'tree)))))))))
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
